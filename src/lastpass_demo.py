9bec1e348831283c382cd79edf2790a507fe386f
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-16 13:02:52 +0200

    feat(lastpass): create lastpass demo

---
+++ src/lastpass_demo.py
@@ -0,0 +1,57 @@
+import os
+
+import lastpass
+
+
+class LastPassManager:
+    """LastPass functions."""
+
+    def __init__(self):
+        self.user = None
+        self.password = None
+        self.vault = None
+
+    def connect(self, user: str, password: str) -> bool:
+        """Connection to LastPass account
+
+        Args:
+            user (str): User for LastPass account
+            password (str): Password for LastPass account
+
+        Returns:
+            bool: True or False
+
+        """
+        self.user = user
+        self.password = password
+        self.vault = lastpass.Vault.open_remote(user, password)
+
+    def get_accounts(self) -> list:
+        """Get all passwords from LastPass account
+
+        Returns:
+            list: Every list item includes numerator dictionary with all password fields
+
+        """
+        accounts = []
+        for i in self.vault.accounts:
+            accounts.append({
+                "id": str(i.id, 'utf-8'),
+                "name": str(i.name, 'utf-8'),
+                "username": str(i.username, 'utf-8'),
+                "password": str(i.password, 'utf-8'),
+                "url": str(i.url, 'utf-8'),
+                "group": str(i.group, 'utf-8'),
+                "notes": str(i.notes, 'utf-8')
+            })
+        return accounts
+
+
+user_name = "xxx"
+password = os.getenv("LASTPASS_PASSWORD")
+my_lp_client = LastPassManager()
+my_lp_client.connect(user_name, password)
+ll = my_lp_client.get_accounts()
+print(len(ll))
+#lastpass.exceptions.LastPassUnknownError: Please review the information you entered and try again.
+
