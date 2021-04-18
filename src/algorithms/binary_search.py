dc40eca307e14e7655bcc7e75c381cbfc19e9639
Xiatong 夏童 <40656281+Magicbeanbuyer@users.noreply.github.com>
2021-04-18 12:33:44 +0000

    add pytest and moved files around

---
+++ src/algorithms/binary_search.py
@@ -0,0 +1,13 @@
+def binary_search(pool: list, target):
+	sorted_pool = sorted(pool)
+	low = 0
+	high = len(sorted_pool) - 1
+	while low + 1 != high:
+		mid = (low + high) // 2
+		if sorted_pool[mid] == target:
+			return mid
+		elif sorted_pool[mid] < target:
+			low = mid
+		else:
+			high = mid
+	return None
---
+++ src/algorithms/remove_nth_node_from_end_of_list.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return f"node value: {self.val}"
+
+
+class Solution:
+
+    @staticmethod
+    def measure_list_length(head: ListNode) -> int:
+        """The method measured the lengh of a list"""
+
+        i = 0
+        temp_node = head
+        while temp_node.next:
+            i += 1
+            temp_node = temp_node.next
+
+        return i + 1
+
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        nth_node_i = n - (i - 1)
+        pass
+
+
+if __name__ == "__main__":
+    linked_list_dict = {f"n{i}": ListNode(i) for i in range(6)}
+    pprint(linked_list_dict)
