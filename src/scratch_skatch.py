5d507b5e04e6b7ed55a76e7efb9d6b7838f5189d
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-20 10:12:48 +0200

    refactor(sratch): delete all codes

--- src/scratch_skatch.py
+++ src/scratch_skatch.py
@@ -1,17 +1,1 @@
-"""Scratch and erase."""
-
-
-def divide(numerator: float, denominator: int) -> float:
-    """Divide and conquer.
-
-    Args:
-        numerator (float): the dividend
-        denominator (int): the divisor, must be integer
-
-    float: the quotient of the division
-    """
-    return numerator / denominator
-
-
-c = divide(numerator=3, denominator=int(4.5))
-print(c)
+"""Put a place holder."""
