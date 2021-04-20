37812b8617fdc62c71082f96c57881a9198d7cea
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-20 16:31:33 +0200

    style(contextmanager): reformat script

--- src/context_manager_demo.py
+++ src/context_manager_demo.py
@@ -1,3 +1,5 @@
+"""Demonstrate context manager."""
+
 from pprint import pprint
 from types import SimpleNamespace
 from zipfile import ZipFile
@@ -8,39 +10,52 @@ base_p = "."
 
 
 def read_file(base_path):
-	file_path = "../resource/sample.yml"
+    """Read a file in a given path.
 
-	if base_path.endswith(".zip"):
-		with ZipFile(base_path) as archive:  # Open the .zip file
-			data = archive.read(file_path)
-			conf_list = yaml.load(data, Loader=yaml.FullLoader)
+    Args:
+        base_path: file path
 
-	else:
-		with open(f"{base_path}/{file_path}") as data:
-			conf_list = yaml.load(data, Loader=yaml.FullLoader)
+    Returns:
+        list: a list of files
+    """
+    file_path = "../resource/sample.yml"
 
-	return conf_list
+    if base_path.endswith(".zip"):
+        with ZipFile(base_path) as archive:  # Open the .zip file
+            data = archive.read(file_path)
+            conf_list = yaml.load(data, Loader=yaml.FullLoader)
 
+    else:
+        with open(f"{base_path}/{file_path}") as data:
+            conf_list = yaml.load(data, Loader=yaml.FullLoader)
 
-def get_schema(base_path) -> str:
-	# try:
-	if base_path.endswith(".zip"):
-		with ZipFile(base_path, 'r') as archive:  # Open the .zip file
-			binary_data = archive.read("schema/aaa.sql")  # Read the CSV => binary format
-			return binary_data.decode("utf-8").replace('\n', ' ').replace('\t', ' ')
-	else:
-		with open("schema/aaa.sql", newline='') as input_file:
-			binary_data = input_file.read()
-			return binary_data.replace('\n', ' ').replace('\t', ' ')
+    return conf_list
 
 
-# except (FileNotFoundError, KeyError):  # schema doesn't exist
-# 	print("External schema does not exist.")
+def get_schema(base_path):
+    """Read schema from file path.
+
+    Args:
+        base_path: the folder that contains a schema folder
+
+    Returns:
+        the schema string
+    """
+    if base_path.endswith(".zip"):
+        with ZipFile(base_path, "r") as archive:  # Open the .zip file
+            binary_data = archive.read(
+                "schema/aaa.sql"
+            )  # Read the CSV => binary format
+            return binary_data.decode("utf-8").replace("\n", " ").replace("\t", " ")
+    else:
+        with open("schema/aaa.sql", newline="") as input_file:
+            binary_data = input_file.read()
+            return binary_data.replace("\n", " ").replace("\t", " ")
 
 
 a = read_file(base_p)
 for i in a:
-	dedup = SimpleNamespace(**i["deduplication"])
-	print(dedup.watermark)
+    dedup = SimpleNamespace(**i["deduplication"])
+    print(dedup.watermark)
 
 pprint(a)
