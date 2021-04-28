e9444940b1a428408d7a0bcfbd57ddfdbbfec0ef
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-28 13:18:55 +0200

    feat: compare global var with local var

--- src/global_variable_demo.py
+++ src/global_variable_demo.py
@@ -1,7 +1,21 @@
 """Demonstrate difference between global and local variable."""
 global_var = "a"
+shadow_global_var = "b"
 
 
 def foo():
-    """Print local and global vairables."""
+    """Print local and global variables."""
     print(f"foo: {global_var}")
+
+
+def bar():
+    """Print local and global variables."""
+    global shadow_global_var
+    shadow_global_var = shadow_global_var * 2
+    print(f"bar: {global_var}\nshadow: {shadow_global_var}")
+
+
+# try:
+foo()
+# except UnboundLocalError:
+bar()
