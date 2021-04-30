04e46b52314b6ce6100ce03d3afdf6e3f5ce1709
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-30 13:59:31 +0200

    ci: fix missing revision commit id

--- .circleci/bin/code_and_commit_linter.sh
+++ .circleci/bin/code_and_commit_linter.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 pipeline_base_revision=$1
-pipeline_revision=$2
 
 # get the ID of the first commit of the branch
 if [ "$pipeline_base_revision" = "" ]; then
   first_commit="$(git log --walk-reflogs "$CIRCLE_BRANCH" --format="%H" | tail -n 2 | head -n 1)"
 else
-  first_commit=${pipeline_base_revision}
+  first_commit="$pipeline_base_revision"
 fi
 
-pre-commit install --hook-type pre-commit
-pre-commit run --hook-stage manuagitl --from-ref "$first_commit" --to-ref "$pipeline_revision"
-cz check --rev-range "$first_commit".."$pipeline_revision"
+branch_head="$(git rev-parse HEAD)"
+
+#pre-commit install --hook-type pre-commit
+pre-commit run --hook-stage manual --from-ref "$first_commit" --to-ref "$branch_head"
+cz check --rev-range "$first_commit".."$branch_head"
