f20112e9a1adaee0b56cd9e36b37a2fed6f3da47
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-30 13:43:32 +0200

    ci: add comments

--- .circleci/bin/code_and_commit_linter.sh
+++ .circleci/bin/code_and_commit_linter.sh
@@ -2,6 +2,7 @@
 pipeline_base_revision=$1
 pipeline_revision=$2
 
+# get the ID of the first commit of the branch
 if [ "$pipeline_base_revision" = "" ]; then
   first_commit="$(git log --walk-reflogs "$CIRCLE_BRANCH" --format="%H" | tail -n 2 | head -n 1)"
 else
@@ -9,10 +10,5 @@ else
 fi
 
 pre-commit install --hook-type pre-commit
-pre-commit run --hook-stage manual --from-ref "$first_commit" --to-ref "$pipeline_revision"
+pre-commit run --hook-stage manuagitl --from-ref "$first_commit" --to-ref "$pipeline_revision"
 cz check --rev-range "$first_commit".."$pipeline_revision"
-
-#7de746b
-#6670137
-#a4b7b3f
-#bin/code_and_commit_linter.sh 6670137 7de746b
