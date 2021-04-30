6606cbc6c38cc8ec0c80d171c86eaa5f45402268
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-30 14:31:00 +0200

    ci: print commit ranges

--- .circleci/bin/code_and_commit_linter.sh
+++ .circleci/bin/code_and_commit_linter.sh
@@ -3,12 +3,15 @@ pipeline_base_revision=$1
 
 # get the ID of the first commit of the branch
 if [ "$pipeline_base_revision" = "" ]; then
-  first_commit="$(git log --walk-reflogs "$CIRCLE_BRANCH" --format="%H" | tail -n 2 | head -n 1)"
+  first_commit="$(git log --walk-reflogs "$CIRCLE_BRANCH" --format="%h" | tail -n 2 | head -n 1)"
 else
   first_commit="$pipeline_base_revision"
 fi
 
-branch_head="$(git rev-parse HEAD)"
+branch_head="$(git rev-parse --short HEAD)"
+
+echo "fist commit: $first_commit"
+echo "branch head: $branch_head"
 
 pre-commit run --hook-stage manual --from-ref "$first_commit" --to-ref "$branch_head"
 
