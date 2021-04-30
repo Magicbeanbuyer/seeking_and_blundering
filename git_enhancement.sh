e3b3f0a6310493990ba37d3c2ec30044b5589564
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-04-30 17:08:05 +0200

    build: use git_enhancement.sh only locally
    git_enhancement.sh was designed to be used both locally and in CircleCI, however CircleCI is migrated to custom docker image, therefore no longer needs git_enhancement.sh.

--- git_enhancement.sh
+++ git_enhancement.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-# default "local" when no argument passed, "circleci" when running in circleci
-runtime=${1-local}
-
-pip install pre-commit commitizen --quiet
+# isntall packages
+pip install pre-commit commitizen black flake8 --quiet
 
 # check installation
 pre-commit --version
 echo "commitizen $(cz version)"
+black --version
+flake8 --version
 
 # install hooks to project_root/.git/hooks
 # pre-commit: python, terraform, json, yaml and bash linting hooks
 pre-commit install --hook-type pre-commit
+# commit-msg: conventional commit linting hook
+pre-commit install --hook-type commit-msg
 
-if [ "$runtime" = "local" ]; then
-  # commit-msg: conventional commit linting hook
-  # only install commit-msg hook via pre-commit locally
-  # commitizen won't be called by pre-commit in CircleCI
-  pre-commit install --hook-type commit-msg
-fi
