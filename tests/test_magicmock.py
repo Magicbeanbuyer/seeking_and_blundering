c12ac7943d5580c3a4710e0ac769e0d0b995533d
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2021-07-29 16:46:56 +0200

    feat(mock): patch with magicmock

--- tests/test_magicmock.py
+++ tests/test_magicmock.py
@@ -5,22 +5,23 @@ from demo_magicmock import get_working_dir, Worker
 
 class TestStringMethods(TestCase):
     def test_patch_return_value(self):
-        with mock.patch("demo_magicmock.os.getcwd", return_value="/mock/patch/dir") as mocked_cwd:
-            assert get_working_dir() == "/mock/patch/dir"
+        with mock.patch(
+            "demo_magicmock.os.getcwd", return_value="/one/mock/patch/dir"
+        ) as mocked_cwd:
+            assert get_working_dir() == "/one/mock/patch/dir"
             mocked_cwd.assert_called()
 
-    @mock.patch("demo_magicmock.os.getcwd", return_value="/decorator/path", scope="function")
+    @mock.patch("demo_magicmock.os.getcwd", return_value="/two/decorator/path")
     def test_call_counts(self, mocky_os):
         get_working_dir()
         mocky_os.assert_called_once()
-        assert get_working_dir() == "/decorator/path"
 
     def test_patching_class(self):
         with mock.patch("demo_magicmock.Helper") as MockHelper:
-            MockHelper.return_value.get_path.return_value = "testing"
+            MockHelper.return_value.get_path.return_value = "/three/testing"
             worker = Worker()
             MockHelper.assert_called_once_with("db")
-            self.assertEqual(worker.work(), "testing")
+            self.assertEqual(worker.work(), "/three/testing")
 
 
 if __name__ == "__main__":
