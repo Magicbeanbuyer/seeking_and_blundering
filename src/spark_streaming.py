017dfa997a77cc74130027b9428a4c886cba73df
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2022-02-14 11:27:44 +0100

    feat: add streaming

--- src/spark_streaming.py
+++ src/spark_streaming.py
@@ -2,57 +2,52 @@
 from pyspark.sql.streaming import StreamingQuery
 from pyspark.sql.types import StructType
 from pyspark.sql import SparkSession
+from pyspark.sql import functions
 import time
-
-spark = (
-    SparkSession.builder.appName("x")
-    .config("spark.hadoop.fs.s3a.path.style.access", True)
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-    .config(
-        "spark.hadoop.fs.s3a.aws.credentials.provider",
-        "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider",
-    )
-    .getOrCreate()
-)
+spark = (SparkSession.builder.appName("x")
+         .config("spark.hadoop.fs.s3a.path.style.access", True)
+         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+         .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider")
+         .getOrCreate())
 
 #%%
 userSchema = StructType().add("name", "string").add("age", "integer")
-df = (
-    spark.readStream.option("sep", ",").schema(userSchema).csv("spark_application/csv")
-)  # Equivalent to format("csv").load("/path/to/directory")
+df = spark \
+    .readStream \
+    .option("sep", ",") \
+    .schema(userSchema) \
+    .csv("spark_application/csv")  # Equivalent to format("csv").load("/path/to/directory")
 
 #%%
-query = (
-    df.writeStream.outputMode("append")
-    .option("checkpointLocation", "delta/stream_write/checkpoint")
-    .format("console")
-    .start()
-)
+query = (df
+        .writeStream
+        .outputMode("append")
+        .option("checkpointLocation", "delta/stream_write/checkpoint")
+        .format("console")
+        .start())
 
 time.sleep(10)
 query.stop()
 
 #%%
-query = (
-    df.writeStream.format("delta")
-    .outputMode("append")
-    .option("checkpointLocation", "delta/kids/_checkpoints/")
-    .toTable("kids")
-)
+query = (df.writeStream
+ .format("delta")
+ .outputMode("append")
+ .option("checkpointLocation", "delta/kids/_checkpoints/")
+ .toTable("kids"))
 
 time.sleep(60)
 
 query.stop()
 
 #%%
-query = (
-    df.writeStream.format("delta")
-    .outputMode("append")
-    .option("checkpointLocation", "delta/people/_checkpoints/etl-from-csv")
-    .partitionBy("name")
-    .start("delta/people")
-)
+query = (df.writeStream
+ .format("delta")
+ .outputMode("append")
+ .option("checkpointLocation", "delta/people/_checkpoints/etl-from-csv")
+ .partitionBy("name")
+ .start("delta/people"))
 
 time.sleep(30)
 
@@ -69,7 +64,11 @@ df_i = df_i.groupBy("name").count()
 
 print(df_i.isStreaming)
 
-query = df_i.writeStream.outputMode("complete").format("console").start()
+query = df_i \
+    .writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .start()
 
 
 time.sleep(20)
@@ -87,9 +86,8 @@ query.stop()
 import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
-
 BUCKET = "deutsche-boerse-xetra-pds"
-s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 files = s3.list_objects_v2(Bucket=BUCKET, Prefix="2022-02-10")
 for i in files["Contents"]:
     print(i["Key"])
@@ -99,35 +97,37 @@ df = spark.read.csv("s3a://deutsche-boerse-xetra-pds/2022-02-10/2022-02-10_BINS_
 df.show()
 
 #%%
-schema = (
-    StructType()
-    .add("isin", "string")
-    .add("mnemonic", "string")
-    .add("securityDesc", "string")
-    .add("securityType", "string")
-    .add("currency", "string")
-    .add("securityID", "long")
-    .add("date", "date")
-    .add("time", "string")
-    .add("startPrice", "float")
-    .add("maxPrice", "float")
-    .add("minPrice", "float")
-    .add("endPrice", "float")
-    .add("tradedVolume", "float")
-    .add("numberOfTrades", "integer")
-)
+schema = (StructType()
+          .add("isin", "string")
+          .add("mnemonic", "string")
+          .add("securityDesc", "string")
+          .add("securityType", "string")
+          .add("currency", "string")
+          .add("securityID", "long")
+          .add("date", "date")
+          .add("time", "string")
+          .add("startPrice", "float")
+          .add("maxPrice", "float")
+          .add("minPrice", "float")
+          .add("endPrice", "float")
+          .add("tradedVolume", "float")
+          .add("numberOfTrades", "integer"))
 
-df = spark.readStream.schema(schema).csv("s3a://deutsche-boerse-xetra-pds/2022-02-10/", header=True)
+df = (spark
+      .readStream
+      .schema(schema)
+      .csv("s3a://deutsche-boerse-xetra-pds/2022-02-10/", header=True))
 
 #%%
-query = (
-    df.writeStream.queryName("stock_price")
-    .format("delta")
-    .trigger(once=True)
-    .outputMode("append")
-    .option("checkpointLocation", "delta/stock_price/checkpoint")
-    .toTable("stock_price")
-)
+query = (df
+        .writeStream
+        .queryName("stock_price")
+        .format("delta")
+        .trigger(once=True)
+        .partitionBy("date")
+        .outputMode("append")
+        .option("checkpointLocation", "delta/stock_price/checkpoint")
+        .start("delta/stock_price/data"))
 
 
 def stop_streaming_query_gracefully(streaming_query: StreamingQuery):
@@ -139,8 +139,31 @@ def stop_streaming_query_gracefully(streaming_query: StreamingQuery):
         print(f"{streaming_query.name} status: {query_status}")
     streaming_query.stop()
 
-
 stop_streaming_query_gracefully(query)
 
 #%%
-spark.table("stock_price").show()
+stock_price = spark.read.format("delta").load("delta/stock_price/data")
+print(stock_price.count())
+stock_price.groupBy("isin").count().show()
+
+#%%
+dk = stock_price.select("isin", "date", "time", "startPrice", "endPrice").where("isin in ('DE000ETFL144', 'DE0006042708')").sort("time")
+dk.show()
+
+#%%
+from pyspark.sql import functions
+# dk.select("*", functions.concat_ws(" ", dk.date, dk.time).alias("datetime")).show()
+dk = dk.withColumn("datetime", functions.to_timestamp(functions.concat_ws(" ", dk.date, dk.time)))
+dk.show()
+# dk.printSchema()
+#%%
+from pyspark.sql import Window
+
+asc_window = Window.partitionBy("isin").orderBy(dk.datetime.asc())
+desc_window = Window.partitionBy("isin").orderBy(dk.datetime.desc())
+start_df = dk.withColumn("asc_rank", functions.rank().over(asc_window)).select("isin", "startPrice").where("asc_rank = 1")
+end_df = dk.withColumn("desc_rank", functions.rank().over(desc_window)).select("isin", "endPrice").where("desc_rank = 1")
+#%%
+price = start_df.join(end_df, on="isin")
+final = price.withColumn("performance", (price.startPrice - price.endPrice)/price.endPrice)
+final.show()
