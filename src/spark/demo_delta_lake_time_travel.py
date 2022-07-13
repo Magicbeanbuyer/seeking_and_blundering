ae821afe875f2b4d8b72cd4fe44a7446fb057186
Xiatong Zheng <xiatongzheng@marc-o-polo.com>
2022-07-13 22:43:02 +0200

    chore: run vacuum

--- src/spark/demo_delta_lake_time_travel.py
+++ src/spark/demo_delta_lake_time_travel.py
@@ -13,18 +13,19 @@ spark = (
 # %% create delta table
 table_name = "friends_v2"
 s3_prefix = "s3a://path-to-s3"
-table_path = f"{s3_prefix}/{table_name}"
+# table_path = f"{s3_prefix}/{table_name}"
+table_path = "delta/vacumm"
 schema = StructType(
     [StructField("id", IntegerType(), False), StructField("name", StringType(), False)]
 )
-delta_table = (
-    DeltaTable.create(spark)
-    .tableName(table_name)
-    .addColumns(schema)
-    .location(table_path)
-    # .property("delta.deletedFileRetentionDuration", "0 hours")
-    .execute()
-)
+# delta_table = (
+#     DeltaTable.create(spark)
+#     .tableName(table_name)
+#     .addColumns(schema)
+#     .location(table_path)
+#     # .property("delta.deletedFileRetentionDuration", "0 hours")
+#     .execute()
+# )
 # write v1
 data = [(1, "Monica"), (2, "Rachel")]
 df = spark.createDataFrame(data, schema)
@@ -37,10 +38,10 @@ df = spark.createDataFrame(data, schema)
 df.write.format("delta").mode("overwrite").save(table_path)
 
 # %% query v1
-df = spark.read.format("delta").option("versionAsOf", "2").load(table_path)
+df = spark.read.format("delta").option("versionAsOf", "0").load(table_path)
 df.show()
 # query v2
-df = spark.read.format("delta").option("versionAsOf", "1").load("delta/people")
+df = spark.read.format("delta").option("versionAsOf", "1").load(table_path)
 df.show()
 # query latest
 df = spark.read.format("delta").load(table_path)
@@ -51,5 +52,9 @@ fullHistoryDF = deltaTable.history()  # get the full history of the table
 fullHistoryDF.show(truncate=False)
 # lastOperationDF = deltaTable.history(1) # get the last operation
 # %% vacuum
-q = spark.sql(f"VACUUM delta.`{table_path}` DRY RUN")
+spark.conf.set("spark.databricks.delta.retentionDurationCheck.enabled", False)
+deltaTable = DeltaTable.forPath(spark, table_path)
+deltaTable.vacuum(0)
+
+# q = spark.sql(f"VACUUM delta.`/delta/vacumm` DRY RUN")
 # Found 3 files and directories in a total of 1 directories that are safe to delete.
